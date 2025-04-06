c1e52666e58a70408379c6bf9079994a345769f9
AgustinRguez <agus_rodriguez-@hotmail.com>
2025-04-06 18:31:03 -0300

    change in the popup

--- map_app/map.py
+++ map_app/map.py
@@ -8,16 +8,17 @@ lon = list(data["LON"])
 data_stadium = pandas.read_csv("estadios.txt")
 lon_stadium = list(data_stadium["long"])
 lat_stadium = list(data_stadium["lat"])
+club = list(data_stadium["club"])
 
-argentina_map = folium.Map(location=[-58.4497663377443,-34.5453056273984], zoom_start=6)
+
+argentina_map = folium.Map(location=[-34.66, -58.36], zoom_start=6)
 fg = folium.FeatureGroup("argentina Maps")
 
 for lt, ln in zip(lat, lon):
     fg.add_child(folium.Marker(location=[lt, ln], popup="First marker", icon=folium.Icon("blue")))
 
-for stadium_lat, stadium_ln in zip(lat_stadium, lon_stadium):
-    fg.add_child(folium.Marker(location=[stadium_lat, stadium_ln], popup="Estadios en CABA", icon=folium.Icon("blue")))
+for stadium_lat, stadium_ln, cl in zip(lat_stadium, lon_stadium, club):
+    fg.add_child(folium.Marker(location=[stadium_lat, stadium_ln], popup="Estadio de " + cl, icon=folium.Icon("blue")))
 
 argentina_map.add_child(fg)
-
 argentina_map.save("argentina_map1.html")
