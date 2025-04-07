ee1ba16db38978e097386f0180a027cde220b472
AgustinRguez <agus_rodriguez-@hotmail.com>
2025-04-07 17:18:36 -0300

    Ultimo commit, app de curso hecho

--- map_app/map.py
+++ map_app/map.py
@@ -12,13 +12,23 @@ club = list(data_stadium["club"])
 
 
 argentina_map = folium.Map(location=[-34.66, -58.36], zoom_start=6)
-fg = folium.FeatureGroup("argentina Maps")
+fg_volcan = folium.FeatureGroup(name="Volcanes yankis")
+fg_estadios = folium.FeatureGroup(name="Canchas de futbol argentino")
+fg_poblacion = folium.FeatureGroup(name="Poblacion")
 
 for lt, ln in zip(lat, lon):
-    fg.add_child(folium.Marker(location=[lt, ln], popup="First marker", icon=folium.Icon("blue")))
+    fg_volcan.add_child(folium.Circle(location=[lt, ln], popup="First marker", icon=folium.Icon("red")))
 
 for stadium_lat, stadium_ln, cl in zip(lat_stadium, lon_stadium, club):
-    fg.add_child(folium.Marker(location=[stadium_lat, stadium_ln], popup="Estadio de " + cl, icon=folium.Icon("blue")))
+    fg_estadios.add_child(folium.Marker(location=[stadium_lat, stadium_ln], popup="Estadio de " + cl, icon=folium.Icon("green")))
 
-argentina_map.add_child(fg)
-argentina_map.save("argentina_map1.html")
+fg_poblacion.add_child(folium.GeoJson(open("world.json", encoding="utf-8-sig").read(),
+style_function=lambda x: {'fillColor': 'green' if x['properties']['POP2005'] < 10000000
+else 'orange' if 10000000 <= x['properties']['POP2005'] < 20000000 else 'red'}))
+
+argentina_map.add_child(fg_volcan)
+argentina_map.add_child(fg_estadios)
+argentina_map.add_child(fg_poblacion)
+argentina_map.add_child(folium.LayerControl())
+
+argentina_map.save("mapa.html")
